90cfe5bc23f0fb6a9a47c1c2aecf30298bc240fa
luckyleck <luckyleck29@gmail.com>
2023-04-04 16:13:00 -0400

    Making Splash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
+import Splash from './components/Splash';
 
 function App() {
   return (
     <Switch>
+      <Route exact path="/">
+        <Splash />
+      </Route>
       <Route path="/login">
         <LoginFormPage /> 
       </Route>
