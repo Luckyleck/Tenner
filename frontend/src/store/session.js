9fbff184befdb470a4b0d5b0df5b4d27f9d23176
luckyleck <luckyleck29@gmail.com>
2023-04-04 12:16:31 -0400

    Up To Interlude

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -4,31 +4,62 @@ const SET_SESSION_USER = 'session/setSessionUser';
 const REMOVE_SESSION_USER = 'session/removeSessionUser';
 
 
-const setSessionUser = (user) => {
+const setCurrentUser = (user) => {
     return {
         type: SET_SESSION_USER,
         payload: user,
     };
 };
 
-const removeSessionUser = () => {
+const removeCurrentUser = () => {
     return {
         type: REMOVE_SESSION_USER,
     };
 };
 
-export function login({credential, password}) {
+function storeCSRFToken(response) {
+    const csrfToken = response.headers.get('X-CSRF-Token');
+    if (csrfToken) {
+        sessionStorage.setItem('X-CSRF-Token', csrfToken);
+    }
+}
+
+function storeCurrentUser(user) {
+    if (user) {
+        sessionStorage.setItem('currentUser', JSON.stringify(user));
+    } else {
+        sessionStorage.removeItem('currentUser');
+    }
+}
+
+export function login({ credential, password }) {
     return (async (dispatch) => {
         const res = await csrfFetch('/api/session', {
             method: 'POST',
             body: JSON.stringify({ credential, password })
         });
         const user = await res.json();
-        dispatch(setSessionUser(user))
+        storeCurrentUser(user)
+        dispatch(setCurrentUser(user))
+        return res;
     })
 }
 
-const initialState = { user: null };
+export function restoreSession () {
+    return (async (dispatch) => {
+        const res = await csrfFetch('/api/session');
+        storeCSRFToken(res);
+
+        const user = await res.json();
+        storeCurrentUser(user);
+        dispatch(setCurrentUser(user));
+        return res;
+    })
+}
+
+const initialState = { 
+    user: JSON.parse(sessionStorage.getItem("currentUser"))
+  };
 
 // Reducer
 const sessionReducer = (state = initialState, action) => {
