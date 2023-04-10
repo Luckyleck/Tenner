4d7fa7c2c546ceacda6025a93a71be9508c2d5c4
chakhoic <chakhoichan@gmail.com>
2023-04-10 17:48:08 -0400

    gigs

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,8 +1,10 @@
 class Gig < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 5 }
-    validates :description, presence: true, length: {
-        minimum: 5, maximum: 25
-    }
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: {
+                            minimum: 5, maximum: 25,
+                          }
 
-    has_many :reviews
+  has_many :reviews,
+           foreign_key: :gig_id,
+           class_name: :Gig
 end
