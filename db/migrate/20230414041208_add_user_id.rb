e710f3d00256e8c30127958733dd0f34912c81db
Alex <luckyleck29@gmail.com>
2023-04-14 00:24:33 -0400

    Change to DB, failed

--- db/migrate/20230414041208_add_user_id.rb
+++
@@ -1,5 +0,0 @@
-class AddUserId < ActiveRecord::Migration[7.0]
-  def change
-    add_column :reviews, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_041208) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_200425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,7 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_041208) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "gig_id"
-    t.integer "user_id"
     t.index ["reviewer_id"], name: "index_reviews_on_reviewer_id"
   end
 
