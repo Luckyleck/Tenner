f127e3495bdb4d84a4df2efe2aa58fc4c123b184
Luckyleck <luckyleck29@gmail.com>
2023-04-24 14:14:26 -0400

    Delete Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
-class Post < ApplicationRecord
-post = Post.create(title: "First post!")
-file = File.open('app/assets/images/<your-filename>.jpg') # <- Adjust the path here if the image is not in app/assets/images
-post.photo.attach(io: file, filename: '<your-filename>.jpg')
-post.photo.attached? # => true
+# class Post < ApplicationRecord
+# post = Post.create(title: "First post!")
+# file = File.open('app/assets/images/<your-filename>.jpg') # <- Adjust the path here if the image is not in app/assets/images
+# post.photo.attach(io: file, filename: '<your-filename>.jpg')
+# post.photo.attached? # => true
 
-end 
+# end 
