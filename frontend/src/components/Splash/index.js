f658fd5847811c7b569ff08531e01eed1c96b9c3
Alex <luckyleck29@gmail.com>
2023-05-29 20:52:01 -0400

    Conditional overflow

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -8,9 +8,10 @@ import GTAvideo from '../../assets/GTA_loading_screens.mp4'
 function Splash() {
 
     const sessionUser = useSelector(state => state.session.user)
+    const shouldHideOverflow = !sessionUser;
 
     return (
-        <div className="main-content">
+        <div className={`main-content ${shouldHideOverflow ? 'overflow-hidden' : ''}`}>
             {
                 sessionUser ?
                     <>
@@ -27,39 +28,10 @@ function Splash() {
                                 services for your business
                             </h1>
                         </div>
-                        {/* <div className="main-text">
-                            <h1>
-                                Find the perfect 
-                                <span className="italic"> freelance </span>
-                                 services for your business
-                            </h1>
-                        </div> */}
                     </>
             }
         </div>
     )
-
-
-    // return (
-    //     <div id='all-page'>
-    //         {/* <div className='header'>
-    //             {/* <Navbar className="nav" /> */}
-    //             {!sessionUser &&
-    //                 <div className="main-text">
-    //                     <h1>Find the perfect <span className="italic">freelance</span> services for your business</h1>
-
-    //                     {/* <img id="ten-pound" src="https://gcdnb.pbrd.co/images/AZpADIf24z6w.png?o=1" alt="10pound"/> */}
-    //                 </div>}
-    //         </div> */}
-    //         {sessionUser &&        
-    //         <div className='main-content'>
-    //             <h2>Our best gigs</h2>
-    //             <GigsList />
-    //         </div>
-    //         }   
-
-    //     </div>
-    // )
 }
 
 export default Splash;
