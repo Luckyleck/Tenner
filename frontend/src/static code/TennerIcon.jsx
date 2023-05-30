8134a95844acf1edaea3bf5c4d132e642af68bc0
Alex <luckyleck29@gmail.com>
2023-05-30 15:35:28 -0400

    Logo editing

--- frontend/src/static code/TennerIcon.jsx
+++ frontend/src/static code/TennerIcon.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function TennerIcon () {
     return (
-        <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
+        <Link id="logo-clicked" to="/" style={{ textDecoration: 'none', color: 'black' }}>
             <img id="logo" src="https://gcdnb.pbrd.co/images/aUgY95TlnT6k.png?o=1" alt="tenner logo"></img>
         </Link>
     )
