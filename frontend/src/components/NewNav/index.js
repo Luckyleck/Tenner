77da0efc2f75e3af9e0096928ed8c8c3f445711a
Alex <luckyleck29@gmail.com>
2023-05-31 20:12:49 -0400

    Creat

--- frontend/src/components/NewNav/index.js
+++ frontend/src/components/NewNav/index.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import * as modalActions from '../../store/modals';
 import * as sessionActions from '../../store/session';
 
-import TennerIcon from '../../static code/TennerIcon';
+import TennerIcon from '../../static_code/TennerIcon';
+import SearchBar from '../Search';
 import SigninModal from '../Modals/SigninModal'
 import JoinModalOne from '../Modals/JoinModal';
 import JoinModalTwo from '../Modals/JoinModal2';
@@ -48,6 +49,7 @@ function NewNav() {
                 sessionUser ?
                     <div className="signed-in-nav">
                         <TennerIcon />
+                        <SearchBar />
                         <button onClick={() => setShowDropDownMenu(!showDropDownMenu)}>
                             {sessionUser.username[0]}
                         </button>
---
+++ frontend/src/components/Search/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SearchBar () {
+
+    return (
+
+
+    )
+
+}
+
+
+
+export default SearchBar;
