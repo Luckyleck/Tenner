087d9a2cc288d8f450c8f0cfe4f74ef0b7339f5d
Alex <luckyleck29@gmail.com>
2023-07-09 14:22:22 -0400

    Fixed search issue

--- frontend/src/components/Search/SearchResultsIndex.js
+++ frontend/src/components/Search/SearchResultsIndex.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import GigCard from '../Splash/GigList/GigCard'
+import './SearchResultsIndexStyles.css'
 
 function SearchResultsIndex() {
-    const { gigs } = useSelector(state => state.search.gigs)
-    const query = useSelector(state => state.search.gigs.query);
+
+    const gigs = useSelector(state => state?.search?.gigs?.gigs);
+    const query = useSelector(state => state?.search?.gigs?.query);
 
     if (!gigs) {
         return <h1>Loading..</h1>
     }
 
     return (
-        <div>
-            <h1>Results for {query}</h1>
-            <div className="gig-card-container">
-            {gigs.map((gig) => (
-                <GigCard key={gig.id} gig={gig} />
-            ))}
+        <>
+            <div className="gig-list-container">
+                {
+                    gigs.length ?
+                        <h4>Results for <span>{query}</span></h4>
+                        :
+                        <h4>Sorry no results for <span>{query}</span></h4>
+                }
+
+                <div className="gig-card-container">
+                    {gigs?.map((gig) => (
+                        <GigCard key={gig.id} gig={gig} />
+                    ))}
+                </div>
             </div>
-        </div>
+            <div className="return-to-all-gigs">
+                <Link to={`/`}>Return to all gigs...</Link>
+            </div>
+
+        </>
+
     )
 }
 
